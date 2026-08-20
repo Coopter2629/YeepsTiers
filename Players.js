3d0921c201eab8205199f96d70eb2e2841a40811
Coopter.2629 <calynch2911@gmail.com>
2026-08-20 00:09:58 -0700

    Update Players.js

--- Players.js
+++ Players.js
@@ -1,1 +1,14 @@
-
+export const players = [
+  {
+    name: "GoobaYeeps",
+    image: "",
+    ranks: {
+      "Shield": "S+",
+      "Dagger": "S+",
+      "Mace": "S+",
+      "Double Bat": "S+",
+      "Freeze Glove": "S+",
+      "SMP": "S+",
+      "Scythe": "S+"
+    }
+  },
