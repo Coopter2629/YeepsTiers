2f34dbebf8147bbb29244278d6da590a28efec7d
Wooperyeeps <woopergorillatag@gmail.com>
2026-08-20 13:57:48 -0400

    Add player data for 'test final' and 'waitstopscrolling'
    Added player data with ranks for two players.

--- Players.js
+++ Players.js
@@ -1,1 +1,30 @@
-
+export const players = [
+  {
+    name: "test final",
+    image: "",
+    ranks: {
+      "Shield": "A",
+      "Dagger": "S",
+      "Mace": "S+",
+      "Double Bat": "A+",
+      "Freeze Glove": "C",
+      "SMP": "D",
+      "Scythe": "D+"
+    }
+  },
+  
+{
+    name: "waitstopscrolling",
+    image: "",
+    ranks: {
+      "Shield": "B",
+      "Dagger": "B+",
+      "Mace": "",
+      "Double Bat": "",
+      "Freeze Glove": "",
+      "SMP": "",
+      "Scythe": ""
+    }
+  },
+  
+ 
