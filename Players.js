49a776c38314e68bb96add1a38e18fcb69bac7fb
Wooperyeeps <woopergorillatag@gmail.com>
2026-08-20 14:24:07 -0400

    Refactor player ranks in Players.js
    Updated player ranks to remove specific values and set some to empty strings.

--- Players.js
+++ Players.js
@@ -3,13 +3,13 @@ export const players = [
     name: "test final",
     image: "",
     ranks: {
-      "Shield": "A-",
-      "Dagger": "S-",
-      "Mace": "S+",
-      "Double Bat": "A+",
-      "Freeze Glove": "C-",
-      "SMP": "D-",
-      "Scythe": "D-"
+      "Shield": "",
+      "Dagger": "",
+      "Mace": "",
+      "Double Bat": "A",
+      "Freeze Glove": "C",
+      "SMP": "",
+      "Scythe": ""
     }
   },
 ];
