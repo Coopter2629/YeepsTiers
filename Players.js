13cb689458b694dfda5648609cc56c0e60bc01c6
Wooperyeeps <woopergorillatag@gmail.com>
2026-08-20 14:21:53 -0400

    Modify player ranks in Players.js
    Updated ranks for Shield, Dagger, Freeze Glove, SMP, and Scythe.

--- Players.js
+++ Players.js
@@ -3,13 +3,13 @@ export const players = [
     name: "test final",
     image: "",
     ranks: {
-      "Shield": "A",
-      "Dagger": "S",
+      "Shield": "A-",
+      "Dagger": "S-",
       "Mace": "S+",
       "Double Bat": "A+",
-      "Freeze Glove": "C",
-      "SMP": "D",
-      "Scythe": "D"
+      "Freeze Glove": "C-",
+      "SMP": "D-",
+      "Scythe": "D-"
     }
   },
 ];
