5c91b94891a809e8dab48999b33046f0c948e40a
Coopter.2629 <calynch2911@gmail.com>
2026-08-24 18:31:00 -0700

    Update image path for HEAVY_96

--- Players.js
+++ Players.js
@@ -58,7 +58,7 @@ export const players = [
   
   {
     name: "HEAVY_96",
-    image: "pfps/Screenshot_20260820_141125_Discord.jpg",
+    image: "pfps/Heavy96.jpg",
     ranks: {
       "Shield": "",
       "Dagger": "C",
